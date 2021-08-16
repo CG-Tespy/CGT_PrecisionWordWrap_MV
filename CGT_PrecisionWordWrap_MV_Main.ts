bba8b3ce51a4233ba989f1497b3e3cc3b71be91d
CG-Tespy <gbcabreracgt@gmail.com>
2021-08-15 20:57:35 -0400

    Added Spanish translation

--- CGT_PrecisionWordWrap_MV_Main.ts
+++ CGT_PrecisionWordWrap_MV_Main.ts
@@ -1,12 +1,28 @@
 /*:
-@plugindesc Wraps based on spaces and the state of the message box.
-@author CG-Tespy – https://github.com/CG-Tespy
-@help This is version 2.01.01 of this plugin. Tested with RMMV versions 1.5.1 and 1.6.2. 
-Requires version 1.01.01+ of the CGT WordWrapCore plugin. To have this set as the wrapper
-right away, set the WordWrapCore's Wrapper param to "precision".
+* @plugindesc Wraps based on the physical space and state of the message box.
+* @author CG-Tespy – https://github.com/CG-Tespy
+* @help This is version 2.01.01 of this plugin. Tested with RMMV versions 1.5.1 and 
+* 1.6.2. 
+* 
+* Requires version 2.01.03+ of the CGT WordWrapCore plugin. To have this 
+* set as the wrapper right away, set the WordWrapCore's Wrapper param to "precision".
+* 
+* This does not yet support desc-wrapping.
+* 
+*/
 
-This does not yet support desc-wrapping.
-
+/*:es
+* @plugindesc Ajuste basado en el espacio físico y el estado del cuadro de diálogo.
+* @author CG-Tespy – https://github.com/CG-Tespy
+* @help Este es la versión 2.01.01 de este plugin. Lo probé con versiones RMMV 
+* 1.5.1 y 1.6.2. 
+* 
+* Necesita versión 2.01.03+ del CGT WordWrapCore (Núcleo de Ajustelíneas) 
+* plugin. Para poner este como el ajustelíneas activo inmediatamente, pon el
+* Ajustelíneas parám del WordWrapCore como "precision".
+* 
+* Este aún no puede ajustar los descripciones.
+* 
 */
 
 import { PrWoWr } from './PrecisionWordWrap/_Setup';
